6878b1ba2672e6b26d8041fa7bc00c48e80cc277
Isabella Nunes <isabella.nunes@philips.com>
2021-02-25 19:30:37 -0300

    Added integration tests

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,3 @@
-import express, { response } from 'express';
-
-const app = express();
-
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World - NLW4' });
-});
-
-app.post('/', (request, response) => {
-    return response.json( {message: 'Os dados foram salvos com sucesse!'} );
-});
+import { app } from "./app";
 
 app.listen(3333, () => console.log('Server is running!'));
