c6f75ef34b6c128bdb27679c2041a386af380b5e
Isabella Nunes <isabella.nunes@philips.com>
2021-03-01 11:42:30 -0300

    Finish all tests

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -2,29 +2,29 @@ import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 
-class SurveysController{
-    async create(request: Request, response: Response) {
-        const { title, description } = request.body;
+class SurveysController {
+  async create(request: Request, response: Response) {
+    const { title, description } = request.body;
 
-        const surveysRepository = getCustomRepository(SurveysRepository);
+    const surveysRepository = getCustomRepository(SurveysRepository);
 
-        const survey = surveysRepository.create({
-            title,
-            description
-        });
+    const survey = surveysRepository.create({
+      title,
+      description,
+    });
 
-        await surveysRepository.save(survey);
+    await surveysRepository.save(survey);
 
-        return response.status(201).json(survey);
-    }
+    return response.status(201).json(survey);
+  }
 
-    async show(request: Request, response: Response) {
+  async show(request: Request, response: Response) {
+    const surveysRepository = getCustomRepository(SurveysRepository);
 
-        const surveysRepository = getCustomRepository(SurveysRepository);
-        const all = await surveysRepository.find();
-        return response.json(all);
-    }
+    const all = await surveysRepository.find();
 
+    return response.json(all);
+  }
 }
 
 export { SurveysController };
